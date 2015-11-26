cf1076ce74bac44b6942fb1bf4350f981808cdef
Micheal Tesfay <mtesfay@spartaglobal.com>
2015-11-26 10:32:08 +0000

    added yaml file, readme file

---
+++ readme.txt
@@ -0,0 +1,5 @@
+Testing tublr using watir webdriver 
+
+testing tumblr login and posting text and checking that the post is desplayed on the blog.
+
+http://watir.com/
--- watir-Exercis.rb
+++ watir-Exercis.rb
@@ -1,23 +1,31 @@
 require 'watir'
-require 'Watir' 
- browser = Watir::Browser.new :firefox  
- browser.goto "https://www.tumblr.com/login"
-singUpEmail = browser.text_field(id: "signup_email").set "nightmar@live.co.uk" 
-singUpPassword = browser.text_field(id: "signup_password").set "example2"
+require 'yaml' 
+
+browser = Watir::Browser.new :firefox  
+ browser.goto "https://www.tumblr.com/login" 
+
+file = YAML.load_file('./userdetails.yml')
+ file["user"].each do |yaml_user|
+ 		browser.text_field(id: "signup_email").send_keys "#{yaml_user["email"]}" 
+        browser.text_field(id: "signup_password").send_keys "#{yaml_user["password"]}"
+    end
+
+#singUpEmail = browser.text_field(id: "signup_email").set "nightmar@live.co.uk" 
+#singUpPassword = browser.text_field(id: "signup_password").set "example2"
 logInButton = browser.button(id: "signup_forms_submit").click
 iconPostText = browser.i(class: "icon_post_text").click
 textBoxSpace = browser.div(class: "editor-richtext").send_keys "Testing something here"
 titleSpace = browser.div(class: "editor").send_keys "hello ruby"
 click_button = browser.button(class: "create_post_button").click
 
-body = browser.div(class: "post_body")
 
-body.each_with_index do |o, post| puts "your post is: #{o.text}"
+body = browser.element(class: "editor-richtext")
+body1 = browser.element(class: "editor")
 
-  if o.text.match /Testing something here/
-    puts  "Post #{post+1}: success"
-  else  
-    puts "Post #{post+1}: failure"
-  end 
- end
+  if (body1.text.match /hello ruby/) && (body.text.match /Testing something here/)
+    puts"Hader #{body} been matched successfully !!!"
+  	puts"paragraph been matched successfully !!!"
+  else
+  	puts"sorry text not been found"
+  end
 
